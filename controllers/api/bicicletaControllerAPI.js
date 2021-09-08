8527f64484d3ffbf99921a68c5723122160e81d9
Fredy <freorozcoloa@gmail.com>
2021-09-08 10:05:28 -0500

    CRUD aplicación está creada

--- controllers/api/bicicletaControllerAPI.js
+++ controllers/api/bicicletaControllerAPI.js
@@ -5,3 +5,20 @@ exports.bicicleta_list = (req, res)=>{
         bicicletas: Bicicletas.allBicis
     })
 }
+
+exports.bicicleta_create = (req, res)=>{
+    var bici = new Bicicletas(req.body.id, req.body.color, req.body.modelo);
+    bici.ubicacion = [req.body.lat, req.body.log]
+
+    Bicicletas.add(bici)
+
+    res.status(200).json({
+        bicicleta:bici
+    })
+}
+
+
+exports.bicicleta_delete = (req, res) => {
+    Bicicletas.removeById(req.body.id)
+    res.status(200).send();
+}
